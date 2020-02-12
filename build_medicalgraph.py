59721f0187fbf1a07201797d44767f5debcede51
Ivan Chien <qyc027@gmail.com>
2020-02-12 20:07:37 +0800

    Update build_medicalgraph.py

--- build_medicalgraph.py
+++ build_medicalgraph.py
@@ -15,8 +15,8 @@ class MedicalGraph:
         self.g = Graph(
             host="127.0.0.1",  # neo4j 搭载服务器的ip地址，ifconfig可获取到
             http_port=7474,  # neo4j 服务器监听的端口号
-            user="lhy",  # 数据库user name，如果没有更改过，应该是neo4j
-            password="lhy123")
+            user="neo4j",  # 数据库user name，如果没有更改过，应该是neo4j
+            password="neo4j")
 
     '''读取文件'''
     def read_nodes(self):
@@ -201,9 +201,9 @@ class MedicalGraph:
     def create_graphrels(self):
         Drugs, Foods, Checks, Departments, Producers, Symptoms, Diseases, disease_infos, rels_check, rels_recommandeat, rels_noteat, rels_doeat, rels_department, rels_commonddrug, rels_drug_producer, rels_recommanddrug,rels_symptom, rels_acompany, rels_category = self.read_nodes()
         self.create_relationship('Disease', 'Food', rels_recommandeat, 'recommand_eat', '推荐食谱')
-        self.create_relationship('Disease', 'Food', rels_noteat, 'no_eat', '忌吃')
-        self.create_relationship('Disease', 'Food', rels_doeat, 'do_eat', '宜吃')
-        self.create_relationship('Department', 'Department', rels_department, 'belongs_to', '属于')
+        # self.create_relationship('Disease', 'Food', rels_noteat, 'no_eat', '忌吃')
+        # self.create_relationship('Disease', 'Food', rels_doeat, 'do_eat', '宜吃')
+        # self.create_relationship('Department', 'Department', rels_department, 'belongs_to', '属于')
         self.create_relationship('Disease', 'Drug', rels_commonddrug, 'common_drug', '常用药品')
         self.create_relationship('Producer', 'Drug', rels_drug_producer, 'drugs_of', '生产药品')
         self.create_relationship('Disease', 'Drug', rels_recommanddrug, 'recommand_drug', '好评药品')
@@ -267,4 +267,6 @@ class MedicalGraph:
 
 if __name__ == '__main__':
     handler = MedicalGraph()
+    handler.create_graphnodes()
+    handler.create_graphrels()
     # handler.export_data()
